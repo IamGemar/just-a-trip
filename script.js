72f1c48bdfdfd21cb1cf002cca415ee22748245a
IamGemar <nasayaogemar24@gmail.com>
2025-07-10 21:05:52 +0800

    Edit styles

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function startScan(){
     const videoContainer = document.getElementById("videoContainer")
     const video = document.getElementById("video")
     const fakeDiv = document.getElementById("fakeDiv")
+    const text = document.getElementById("textContainer")
     alert("One virus was successfully scanned!")
+    text.style.display = "none"
     fakeDiv.style.display = "none"
     videoContainer.style.display = "block"
     video.style.width = "100vw"
