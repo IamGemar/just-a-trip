472c196a62012ffa10f82d1466bb939df252467b
IamGemar <nasayaogemar24@gmail.com>
2025-07-10 08:31:16 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function startScan(){
+    const videoContainer = document.getElementById("videoContainer")
+    const video = document.getElementById("video")
+    const fakeDiv = document.getElementById("fakeDiv")
+    alert("One virus was successfully scanned!")
+    fakeDiv.style.display = "none"
+    videoContainer.style.display = "block"
+    video.play()
+}
